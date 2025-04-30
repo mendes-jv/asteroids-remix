ee5b9940e9f3f47325d08f859c04b1b9983e008e
João Mendes <mendes.silva.jv@gmail.com>
2025-04-30 03:57:30 -0300

    feat(player): add rotation and keyboard controls for movement
    Signed-off-by: João Mendes <mendes.silva.jv@gmail.com>

--- player.py
+++ player.py
@@ -1,24 +1,32 @@
 import pygame.draw
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0
+        self.__rotation = 0
 
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        forward = pygame.Vector2(0, 1).rotate(self.__rotation)
+        right = pygame.Vector2(0, 1).rotate(self.__rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    def rotate(self, dt):
+        self.__rotation += PLAYER_TURN_SPEED * dt
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
-        pass
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)

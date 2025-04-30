8b8a041e9ff5402aaee8277a492717065f17309c
João Mendes <mendes.silva.jv@gmail.com>
2025-04-30 04:06:26 -0300

    feat(player): add movement functionality with speed constant
    Signed-off-by: João Mendes <mendes.silva.jv@gmail.com>

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame.draw
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 
 class Player(CircleShape):
@@ -20,6 +20,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.__rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.__rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
@@ -30,3 +34,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
